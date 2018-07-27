eda8ac1dec9a54fc00426115d907410f43504738
Noah Colvin <noahcolvin@gmail.com>
2018-07-26 22:30:20 -0400

    adding sample project

---
+++ TestUwp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TestUwp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static DependencyProperty DisplayMaskProperty = DependencyProperty.Register("DisplayMask", typeof(bool), typeof(MainPage), new PropertyMetadata(true));
+        public static DependencyProperty DisplaySsnProperty = DependencyProperty.Register("DisplaySsn", typeof(bool), typeof(MainPage), new PropertyMetadata(false));
+
+        public bool DisplayToggle { set { SetValue(DisplayMaskProperty, value); SetValue(DisplaySsnProperty, !value); } }
+    }
+}
